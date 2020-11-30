f67d143c0373aba729dace06a8146dd35ab599f8
Miguel Fazenda <miguel.fazenda2000@gmail.com>
2020-11-30 23:59:32 +0000

    ver debugg list_activity

--- app/src/main/java/pt/isec/amov/tp_amov/MainActivity.kt
+++ app/src/main/java/pt/isec/amov/tp_amov/MainActivity.kt
@@ -1,11 +1,29 @@
 package pt.isec.amov.tp_amov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_dialog.view.*
+import pt.isec.amov.tp_amov.Dados.DadosLista
+import pt.isec.amov.tp_amov.Dados.DadosProduto
+import pt.isec.amov.tp_amov.Dados.Produtos
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, ListActivity::class.java)
+        startActivity(intent)
     }
+
 }
